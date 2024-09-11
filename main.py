8e05adda980f9bb6c2d9180ef5dc9efa48c4daca
omer1C <omer80004@gmail.com>
2024-09-11 14:15:25 +0300

    main func

--- main.py
+++ main.py
@@ -20,13 +20,15 @@ def main(args):
         print('Sorry plant type is not correct, please try bell_pepper or arbidiopsis.')
 
 if __name__ == "__main__":
+    default_save_path = os.getcwd()
+    default_save_path = os.path.join(default_save_path, 'example')
     parser = argparse.ArgumentParser(description="Analyzing soil and plant quality through segmentation",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     # General settings.
     parser.add_argument("--plant_type", type=str, default="bell_pepper",
                         help="Please enter bell_pepper or arbidiopsis to activate the code.")
-    parser.add_argument("--save_path", type=str,
+    parser.add_argument("--save_path", type=str, default=default_save_path,
                         help="Please enter the location you want to save the results.")
     args = parser.parse_args()
     main(args)
